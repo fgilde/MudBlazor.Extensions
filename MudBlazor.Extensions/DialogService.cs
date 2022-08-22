bbfbc5fac47ab91e031907c1df1b7cd1810e768e
Florian Gilde <fgilde@gmail.com>
2022-08-22 10:10:19 +0200

    ChipSelect read only

--- MudBlazor.Extensions/DialogService.cs
+++ MudBlazor.Extensions/DialogService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using MudBlazor.Extensions.Helper;
 using MudBlazor.Extensions.Options;
@@ -30,7 +26,7 @@ namespace MudBlazor.Extensions
             return await dialogService.Show(type, title, parameters, options).InjectOptionsAsync(options);
         }
 
-        public static async Task<bool?> ShowMessageBoxEx(IDialogService dialogService, MessageBoxOptions mboxOptions, DialogOptionsEx options = null)
+        public static async Task<bool?> ShowMessageBoxEx(this IDialogService dialogService, MessageBoxOptions mboxOptions, DialogOptionsEx options = null)
         {
             DialogParameters parameters = new DialogParameters()
             {
