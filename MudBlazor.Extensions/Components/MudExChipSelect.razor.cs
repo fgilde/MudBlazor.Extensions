677ad043543dfaee8dec754c6630775177d53f43
Florian Gilde <fgilde@gmail.com>
2022-08-26 17:27:00 +0200

    Add variant to chip select

--- MudBlazor.Extensions/Components/MudExChipSelect.razor.cs
+++ MudBlazor.Extensions/Components/MudExChipSelect.razor.cs
@@ -6,6 +6,7 @@ namespace MudBlazor.Extensions.Components;
 
 public partial class MudExChipSelect<T>
 {
+    [Parameter] public Variant Variant { get; set; }
     [Parameter] public Adornment Adornment { get; set; } = Adornment.End;
     [Parameter] public bool ReadOnly { get; set; }
     [Parameter] public Expression<Func<T>>? For { get; set; }
