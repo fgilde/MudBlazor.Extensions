82ebf3302da83289f40a276b0edecbd382174ece
Florian Gilde <fgilde@gmail.com>
2023-03-02 11:12:53 +0100

    . test build again

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 7.0 -InstallDir ./dotnet7
 ./dotnet7/dotnet --version
-./dotnet7/dotnet publish -c Release -o output
+./dotnet7/dotnet publish -c Release -o output ./Samples/MainSample.WebAssembly/MainSample.WebAssembly.csproj
