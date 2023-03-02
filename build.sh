7f86d14d6c1d24cdcd877ec04351395d77eefeb3
Florian Gilde <fgilde@gmail.com>
2023-03-02 11:27:41 +0100

    Change build script

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 7.0 -InstallDir ./dotnet7
 ./dotnet7/dotnet --version
+./dotnet7/dotnet restore
 ./dotnet7/dotnet publish -c Release -o output ./Samples/MainSample.WebAssembly/MainSample.WebAssembly.csproj
