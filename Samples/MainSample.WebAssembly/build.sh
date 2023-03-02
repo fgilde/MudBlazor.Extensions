ad18fac1407603f62aed7dfc774097578095eeba
Florian Gilde <fgilde@gmail.com>
2023-03-02 11:01:21 +0100

    test build

--- Samples/MainSample.WebAssembly/build.sh
+++
@@ -1,6 +0,0 @@
-﻿#!/bin/sh
-curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
-chmod +x dotnet-install.sh
-./dotnet-install.sh -c 7.0 -InstallDir ./dotnet6
-./dotnet6/dotnet --version
-./dotnet6/dotnet publish -c Release -o output
---
+++ build.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/sh
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh -c 7.0 -InstallDir ./dotnet7
+./dotnet7/dotnet --version
+./dotnet7/dotnet publish -c Release -o output
