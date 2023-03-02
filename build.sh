d0b69df9a2639aa27b8fa6ad0940d222d20d29e8
Florian Gilde <fgilde@gmail.com>
2023-03-02 11:29:42 +0100

    .

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 7.0 -InstallDir ./dotnet7
 ./dotnet7/dotnet --version
 ./dotnet7/dotnet restore
+./dotnet7/dotnet build
 ./dotnet7/dotnet publish -c Release -o output ./Samples/MainSample.WebAssembly/MainSample.WebAssembly.csproj
