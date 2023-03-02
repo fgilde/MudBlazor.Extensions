5c513f1b529a275b2d3866fa652bfe804310f72f
Florian Gilde <fgilde@gmail.com>
2023-03-02 11:50:46 +0100

    Deploy sample cloudflare script

---
+++ Samples/MainSample.WebAssembly/deploy.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/sh
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh -c 7.0 -InstallDir ./dotnet7
+./dotnet7/dotnet --version
+./dotnet7/dotnet restore
+./dotnet7/dotnet build
+./dotnet7/dotnet publish -c Release -o output MainSample.WebAssembly.csproj
