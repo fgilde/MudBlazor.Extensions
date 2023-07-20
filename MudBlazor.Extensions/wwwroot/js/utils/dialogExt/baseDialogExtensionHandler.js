7d1f3c826cf5ed099f831eda64bbca34bb5afba3
fgilde <florian.gilde@sabio.de>
2023-07-20 10:59:29 +0200

    Fix JavaScript null exception on dialog title (#28)
    This commit resolves a null exception in 'baseDialogExtensionHandler.js' and 'mudBlazorExtensions.all.js'. The issue was occurring when 'dialogTitleEl' was null and we were trying to access innerText property of it. Now, we ensure 'dialogTitleEl' is not null before accessing its property, thus, preventing the null exception. Minor formatting changes have also been made in '.DotSettings.user'.

--- MudBlazor.Extensions/wwwroot/js/utils/dialogExt/baseDialogExtensionHandler.js
+++ MudBlazor.Extensions/wwwroot/js/utils/dialogExt/baseDialogExtensionHandler.js
@@ -84,7 +84,7 @@
         if (this.dialog) {
             this.dialogHeader = this.dialog.querySelector(this.mudDialogHeaderSelector);
             this.dialogTitleEl = this.dialog.querySelector('.mud-dialog-title');
-            this.dialogTitle = this.dialogTitleEl.innerText.trim();
+            this.dialogTitle = this.dialogTitleEl ? this.dialogTitleEl.innerText.trim() : '';
             this.dialogId = this.dialog.id;
             this.dialogContainerReference = this.dialog.parentElement;
             this.dialogOverlay = this.dialogContainerReference.querySelector('.mud-overlay');
