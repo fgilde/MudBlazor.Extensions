b668d74c264c10e4e9d827fcc91c4a5ec7bfa12f
Florian Gilde <fgilde@gmail.com>
2023-07-26 13:15:43 +0200

    Test sample

---
+++ MudBlazor.Extensions.Tests/UnitTests/Tests/Components/MudExIconPicker.cs
@@ -0,0 +1,20 @@
+﻿
+using Bunit;
+using MudBlazor.Extensions.Components;
+
+namespace MudBlazor.Extensions.Tests.UnitTests.Tests.Components;
+
+public class MudExIconPickerTest
+{
+    //[Fact]
+    //public void ShouldDisplaysIcons()
+    //{
+    //    using var context = new TestContext();
+    //    var cut = context.RenderComponent<MudExIconPicker>();
+
+    //    cut.Find("button").Click();
+
+    //    cut.Find("p").MarkupMatches("<p>Current count: 1</p>");
+    //    Assert.Equal("Current count: 1", cut.Find("p").TextContent);
+    //}
+}
---
+++ MudBlazor.Extensions/Helper/JsRuntimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace MudBlazor.Extensions.Helper;
+
+/// <summary>
+/// Contains extension methods for IJSRuntime to call MudBlazor extensions JavaScript functions.
+/// </summary>
+public static class JsRuntimeExtensions
+{
+    /// <summary>
+    /// Checks if given mouse args are within given element.
+    /// </summary>
+    public static Task<bool> IsWithin(this IJSRuntime runtime, MouseEventArgs args, ElementReference element)
+    {
+        return runtime.InvokeAsync<bool>("MudExEventHelper.isWithin", args, element).AsTask();
+    }
+}
