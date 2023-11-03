1b2a62d1ec0e009237ae9dc3441b607b975cf37a
Florian Gilde <fgilde@gmail.com>
2023-11-03 13:54:59 +0100

    Syntax fix

--- Samples/Samples.Shared/AppIds.cs
+++ Samples/Samples.Shared/AppIds.cs
@@ -2,15 +2,16 @@
 {
     public class AppIds
     {
-    public static string DropBox => GetVar("DROPBOX_APP_ID");
-    public static string Google => GetVar("GOOGLE_CLIENT_ID");
-    public static string OneDrive => GetVar("ONEDRIVE_APP_ID");
+        public static string DropBox => GetVar("DROPBOX_APP_ID");
+        public static string Google => GetVar("GOOGLE_CLIENT_ID");
+        public static string OneDrive => GetVar("ONEDRIVE_APP_ID");
 
-    private static string GetVar(string name)
-    {
-        return Environment.GetEnvironmentVariable(name) 
-               ?? Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Machine) 
-               ?? Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.User) 
-               ?? Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+        private static string GetVar(string name)
+        {
+            return Environment.GetEnvironmentVariable(name)
+                   ?? Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Machine)
+                   ?? Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.User)
+                   ?? Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
         }
+    }
 }
