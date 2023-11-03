0bf7fc1c0e2a3871805143c87dc02a0d2d7326c2
Florian Gilde <fgilde@gmail.com>
2023-11-03 13:50:31 +0100

    Change appIds

--- Samples/Samples.Shared/AppIds.cs
+++ Samples/Samples.Shared/AppIds.cs
@@ -2,8 +2,15 @@
 {
     public class AppIds
     {
-        public const string DropBox = "2ak2m6cfpdeb9f1";
-        public const string Google = "787005879852-vkv0cduhl70u087pq4a8s2jtkdgv1n6s.apps.googleusercontent.com";
-        public const string OneDrive = "55d00a29-1bb6-40bf-90a6-ecd689a52a51";
-    }
+    public static string DropBox => GetVar("DROPBOX_APP_ID");
+    public static string Google => GetVar("GOOGLE_CLIENT_ID");
+    public static string OneDrive => GetVar("ONEDRIVE_APP_ID");
+
+    private static string GetVar(string name)
+    {
+        return Environment.GetEnvironmentVariable(name) 
+               ?? Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Machine) 
+               ?? Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.User) 
+               ?? Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+        }
 }
