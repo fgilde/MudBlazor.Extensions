49087b1c1ab90237cde2f152e0ad621e2d6716df
Florian Gilde <fgilde@gmail.com>
2024-11-18 13:38:06 +0100

    Remove static files reference and http middleware extension

--- MudBlazor.Extensions/MudBlazorExtensionsMiddlewareExtensions.cs
+++ MudBlazor.Extensions/MudBlazorExtensionsMiddlewareExtensions.cs
@@ -1,20 +1,20 @@
-﻿using Microsoft.AspNetCore.Builder;
-using MudBlazor.Extensions.Middleware;
+﻿//using Microsoft.AspNetCore.Builder;
+//using MudBlazor.Extensions.Middleware;
 
-namespace MudBlazor.Extensions;
+//namespace MudBlazor.Extensions;
 
-/// <summary>
-/// MudBlazorExtensionsMiddlewareExtensions
-/// </summary>
-public static class MudBlazorExtensionsMiddlewareExtensions
-{
-    /// <summary>
-    /// Adds the MudBlazorExtensionsMiddleware
-    /// </summary>
-    /// <param name="builder"></param>
-    /// <returns></returns>
-    public static IApplicationBuilder UseMudExtensions(this IApplicationBuilder builder)
-    {
-        return builder.UseMiddleware<MudBlazorExtensionsMiddleware>();
-    }
-}
+///// <summary>
+///// MudBlazorExtensionsMiddlewareExtensions
+///// </summary>
+//public static class MudBlazorExtensionsMiddlewareExtensions
+//{
+//    /// <summary>
+//    /// Adds the MudBlazorExtensionsMiddleware
+//    /// </summary>
+//    /// <param name="builder"></param>
+//    /// <returns></returns>
+//    public static IApplicationBuilder UseMudExtensions(this IApplicationBuilder builder)
+//    {
+//        return builder.UseMiddleware<MudBlazorExtensionsMiddleware>();
+//    }
+//}
