5663391ea007c43675b38741b19ad4ef7c8c8446
Florian Gilde <florian.gilde@cargonerds.com>
2024-12-15 13:36:57 +0100

    Fix #112 for mudblazor 8

--- MudBlazor.Extensions/Helper/MudDialogInstanceExtensions.cs
+++ MudBlazor.Extensions/Helper/MudDialogInstanceExtensions.cs
@@ -4,33 +4,33 @@ namespace MudBlazor.Extensions.Helper;
 
 public static class MudDialogInstanceExtensions
 {
-    public static void CloseAnimated(this IMudDialogInstance instance, IJSRuntime jsRuntime = null) 
-        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Close());
+    public static void CloseAnimated(this IMudDialogInstance instance, IJSRuntime jsRuntime = null)
+        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Close(), TaskScheduler.FromCurrentSynchronizationContext());
 
-    public static void CancelAnimated(this IMudDialogInstance instance, IJSRuntime jsRuntime = null) 
-        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Cancel());
+    public static void CancelAnimated(this IMudDialogInstance instance, IJSRuntime jsRuntime = null)
+        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Cancel(), TaskScheduler.FromCurrentSynchronizationContext());
 
-    public static void CloseAnimated(this IMudDialogInstance instance, DialogResult result, IJSRuntime jsRuntime = null) 
-        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Close(result));
+    public static void CloseAnimated(this IMudDialogInstance instance, DialogResult result, IJSRuntime jsRuntime = null)
+        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Close(result), TaskScheduler.FromCurrentSynchronizationContext());
 
-    public static void CloseAnimated<T>(this IMudDialogInstance instance, T result, IJSRuntime jsRuntime = null) 
-        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Close<T>(result));
+    public static void CloseAnimated<T>(this IMudDialogInstance instance, T result, IJSRuntime jsRuntime = null)
+        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Close<T>(result), TaskScheduler.FromCurrentSynchronizationContext());
 
     public static void CloseAnimatedIf(this IMudDialogInstance instance, IJSRuntime jsRuntime = null)
-        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Close());
+        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Close(), TaskScheduler.FromCurrentSynchronizationContext());
 
     public static void CancelAnimatedIf(this IMudDialogInstance instance, IJSRuntime jsRuntime = null)
-        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Cancel());
+        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Cancel(), TaskScheduler.FromCurrentSynchronizationContext());
 
     public static void CloseAnimatedIf(this IMudDialogInstance instance, DialogResult result, IJSRuntime jsRuntime = null)
-        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Close(result));
+        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Close(result), TaskScheduler.FromCurrentSynchronizationContext());
 
     public static void CloseAnimatedIf<T>(this IMudDialogInstance instance, T result, IJSRuntime jsRuntime = null)
-        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Close<T>(result));
+        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Close<T>(result), TaskScheduler.FromCurrentSynchronizationContext());
 
     private static Task AnimateClose(this IMudDialogInstance instance, IJSRuntime jsRuntime = null, bool checkOptions = false)
-    {        
+    {
         var dialogId = DialogReferenceExtensions.PrepareDialogId(instance.Id);
-        return (jsRuntime ?? JsImportHelper.GetInitializedJsRuntime()).InvokeVoidAsync("MudBlazorExtensions.closeDialogAnimated", dialogId, checkOptions).AsTask();        
+        return (jsRuntime ?? JsImportHelper.GetInitializedJsRuntime()).InvokeVoidAsync("MudBlazorExtensions.closeDialogAnimated", dialogId, checkOptions).AsTask();
     }
 }
