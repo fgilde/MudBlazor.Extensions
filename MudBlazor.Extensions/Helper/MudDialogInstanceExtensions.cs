a0808094f354f1cc7923627ec3db24c09452daf4
Florian Gilde <florian.gilde@cargonerds.com>
2024-12-15 13:34:01 +0100

    fix #112

--- MudBlazor.Extensions/Helper/MudDialogInstanceExtensions.cs
+++ MudBlazor.Extensions/Helper/MudDialogInstanceExtensions.cs
@@ -4,33 +4,33 @@ namespace MudBlazor.Extensions.Helper;
 
 public static class MudDialogInstanceExtensions
 {
-    public static void CloseAnimated(this MudDialogInstance instance, IJSRuntime jsRuntime = null) 
-        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Close());
+    public static void CloseAnimated(this MudDialogInstance instance, IJSRuntime jsRuntime = null)
+        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Close(), TaskScheduler.FromCurrentSynchronizationContext());
 
-    public static void CancelAnimated(this MudDialogInstance instance, IJSRuntime jsRuntime = null) 
-        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Cancel());
+    public static void CancelAnimated(this MudDialogInstance instance, IJSRuntime jsRuntime = null)
+        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Cancel(), TaskScheduler.FromCurrentSynchronizationContext());
 
-    public static void CloseAnimated(this MudDialogInstance instance, DialogResult result, IJSRuntime jsRuntime = null) 
-        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Close(result));
+    public static void CloseAnimated(this MudDialogInstance instance, DialogResult result, IJSRuntime jsRuntime = null)
+        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Close(result), TaskScheduler.FromCurrentSynchronizationContext());
 
-    public static void CloseAnimated<T>(this MudDialogInstance instance, T result, IJSRuntime jsRuntime = null) 
-        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Close<T>(result));
+    public static void CloseAnimated<T>(this MudDialogInstance instance, T result, IJSRuntime jsRuntime = null)
+        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Close<T>(result), TaskScheduler.FromCurrentSynchronizationContext());
 
     public static void CloseAnimatedIf(this MudDialogInstance instance, IJSRuntime jsRuntime = null)
-        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Close());
+        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Close(), TaskScheduler.FromCurrentSynchronizationContext());
 
     public static void CancelAnimatedIf(this MudDialogInstance instance, IJSRuntime jsRuntime = null)
-        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Cancel());
+        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Cancel(), TaskScheduler.FromCurrentSynchronizationContext());
 
     public static void CloseAnimatedIf(this MudDialogInstance instance, DialogResult result, IJSRuntime jsRuntime = null)
-        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Close(result));
+        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Close(result), TaskScheduler.FromCurrentSynchronizationContext());
 
     public static void CloseAnimatedIf<T>(this MudDialogInstance instance, T result, IJSRuntime jsRuntime = null)
-        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Close<T>(result));
+        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Close<T>(result), TaskScheduler.FromCurrentSynchronizationContext());
 
     private static Task AnimateClose(this MudDialogInstance instance, IJSRuntime jsRuntime = null, bool checkOptions = false)
-    {        
+    {
         var dialogId = DialogReferenceExtensions.PrepareDialogId(instance.Id);
-        return (jsRuntime ?? JsImportHelper.GetInitializedJsRuntime()).InvokeVoidAsync("MudBlazorExtensions.closeDialogAnimated", dialogId, checkOptions).AsTask();        
+        return (jsRuntime ?? JsImportHelper.GetInitializedJsRuntime()).InvokeVoidAsync("MudBlazorExtensions.closeDialogAnimated", dialogId, checkOptions).AsTask();
     }
 }
