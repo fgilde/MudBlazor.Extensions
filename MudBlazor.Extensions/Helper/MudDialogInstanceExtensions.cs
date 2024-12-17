b9fec9f30310d2c91f5c78a8d810d59b35b7dd9c
Florian Gilde <florian.gilde@cargonerds.com>
2024-12-17 13:26:25 +0100

    try improve close

--- MudBlazor.Extensions/Helper/MudDialogInstanceExtensions.cs
+++ MudBlazor.Extensions/Helper/MudDialogInstanceExtensions.cs
@@ -5,32 +5,33 @@ namespace MudBlazor.Extensions.Helper;
 public static class MudDialogInstanceExtensions
 {
     public static void CloseAnimated(this MudDialogInstance instance, IJSRuntime jsRuntime = null)
-        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Close(), TaskScheduler.FromCurrentSynchronizationContext());
+        => _ = AnimateClose(instance, (i) => { i.Close(); return Task.CompletedTask; }, jsRuntime);
 
     public static void CancelAnimated(this MudDialogInstance instance, IJSRuntime jsRuntime = null)
-        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Cancel(), TaskScheduler.FromCurrentSynchronizationContext());
+        => _ = AnimateClose(instance, (i) => { i.Cancel(); return Task.CompletedTask; }, jsRuntime);
 
     public static void CloseAnimated(this MudDialogInstance instance, DialogResult result, IJSRuntime jsRuntime = null)
-        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Close(result), TaskScheduler.FromCurrentSynchronizationContext());
-
+        => _ = AnimateClose(instance, (i) => { i.Close(result); return Task.CompletedTask; }, jsRuntime);
     public static void CloseAnimated<T>(this MudDialogInstance instance, T result, IJSRuntime jsRuntime = null)
-        => AnimateClose(instance, jsRuntime).ContinueWith(_ => instance.Close<T>(result), TaskScheduler.FromCurrentSynchronizationContext());
+        => _ = AnimateClose(instance, (i) => { i.Close<T>(result); return Task.CompletedTask; }, jsRuntime);
 
     public static void CloseAnimatedIf(this MudDialogInstance instance, IJSRuntime jsRuntime = null)
-        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Close(), TaskScheduler.FromCurrentSynchronizationContext());
+        => _ = AnimateClose(instance, (i) => { i.Close(); return Task.CompletedTask; }, jsRuntime, true);
 
     public static void CancelAnimatedIf(this MudDialogInstance instance, IJSRuntime jsRuntime = null)
-        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Cancel(), TaskScheduler.FromCurrentSynchronizationContext());
+        => _ = AnimateClose(instance, (i) => { i.Cancel(); return Task.CompletedTask; }, jsRuntime, true);
 
     public static void CloseAnimatedIf(this MudDialogInstance instance, DialogResult result, IJSRuntime jsRuntime = null)
-        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Close(result), TaskScheduler.FromCurrentSynchronizationContext());
+        => _ = AnimateClose(instance, (i) => { i.Close(result); return Task.CompletedTask; }, jsRuntime, true);
 
     public static void CloseAnimatedIf<T>(this MudDialogInstance instance, T result, IJSRuntime jsRuntime = null)
-        => AnimateClose(instance, jsRuntime, true).ContinueWith(_ => instance.Close<T>(result), TaskScheduler.FromCurrentSynchronizationContext());
+        => _ = AnimateClose(instance, (i) => { i.Close<T>(result); return Task.CompletedTask; }, jsRuntime, true);
 
-    private static Task AnimateClose(this MudDialogInstance instance, IJSRuntime jsRuntime = null, bool checkOptions = false)
+    private static async Task AnimateClose(this MudDialogInstance instance, Func<MudDialogInstance, Task> callback, IJSRuntime jsRuntime = null, bool checkOptions = false)
     {
         var dialogId = DialogReferenceExtensions.PrepareDialogId(instance.Id);
-        return (jsRuntime ?? JsImportHelper.GetInitializedJsRuntime()).InvokeVoidAsync("MudBlazorExtensions.closeDialogAnimated", dialogId, checkOptions).AsTask();
+        await (jsRuntime ?? JsImportHelper.GetInitializedJsRuntime()).InvokeVoidAsync("MudBlazorExtensions.closeDialogAnimated", dialogId, checkOptions);
+        await callback(instance);
     }
+
 }
