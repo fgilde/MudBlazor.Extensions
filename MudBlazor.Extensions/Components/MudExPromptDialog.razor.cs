b124a51519c44bf98d492fa33d458362f73dab3c
Florian Gilde <fgilde@gmail.com>
2025-09-17 16:48:05 +0200

    Add button and variant options for prompt dialog

--- MudBlazor.Extensions/Components/MudExPromptDialog.razor.cs
+++ MudBlazor.Extensions/Components/MudExPromptDialog.razor.cs
@@ -35,10 +35,28 @@ public partial class MudExPromptDialog
     public string OkText { get; set; } = "Ok";
 
     /// <summary>
-    /// Color for icon
+    /// Color for Ok Button 
     /// </summary>
     [Parameter, SafeCategory("Appearance")]
-    public MudExColor OkColor { get; set; } = Color.Error;
+    public Color OkColor { get; set; } = Color.Error;
+
+    /// <summary>
+    /// Color for Cancel Button 
+    /// </summary>
+    [Parameter, SafeCategory("Appearance")]
+    public Color CancelColor { get; set; } = Color.Default;
+
+    /// <summary>
+    /// Variant for Ok Button 
+    /// </summary>
+    [Parameter, SafeCategory("Appearance")]
+    public Variant OkVariant { get; set; } = Variant.Filled;
+
+    /// <summary>
+    /// Variant for Cancel Button 
+    /// </summary>
+    [Parameter, SafeCategory("Appearance")]
+    public Variant CancelVariant { get; set; } = Variant.Text;
 
     /// <summary>
     /// The text of the "Cancel" button.
