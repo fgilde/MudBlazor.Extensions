799de6fc0edb12083a1bdae471166826a435303c
Florian Gilde <fgilde@gmail.com>
2025-10-16 19:37:16 +0200

    change build script cloudflare

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet8
 ./dotnet8/dotnet --version
 ./dotnet8/dotnet restore
-./dotnet8/dotnet build
+./dotnet8/dotnet build ./Samples/MainSample.WebAssembly/MainSample.WebAssembly.csproj
 ./dotnet8/dotnet publish -c Release -o output ./Samples/MainSample.WebAssembly/MainSample.WebAssembly.csproj
