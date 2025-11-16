0d140c862e2de92251bf701485341a8c9e3211db
Florian Gilde <fgilde@gmail.com>
2025-11-16 13:47:19 +0100

    -

---
+++ Samples/MainSample.WebAssembly/Pages/MudExDemoPage.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace MainSample.WebAssembly.Pages;
+
+public partial class MudExDemoPage : IMudExDemoRegistrar
+{
+    
+}
+
+
+public interface IMudExDemoRegistrar
+{
+    void Register(MudExDemoSection section);
+    void Unregister(string id);
+}
+
+public class MudExDemoSection
+{
+    public string Id { get; set; } = default!;
+    public string Title { get; set; } = default!;
+    public int Order { get; set; }          // Reihenfolge in der Nav
+}
